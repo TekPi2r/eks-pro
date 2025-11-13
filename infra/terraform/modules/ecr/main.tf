39018ae0b59254052dcdeb51214d4113c38ef0a6
Pierre <pierre1.dallara@epitech.eu>
2025-11-13 10:12:54 +0100

    fix(poc-02): adjust ECR lifecycle priorities

--- infra/terraform/modules/ecr/main.tf
+++ infra/terraform/modules/ecr/main.tf
@@ -21,22 +21,22 @@ resource "aws_ecr_lifecycle_policy" "keep_recent" {
     rules = [
       {
         rulePriority = 1,
-        description  = "Keep last ${var.retain_images} images",
-        selection = {
-          tagStatus   = "any",
-          countType   = "imageCountMoreThan",
-          countNumber = var.retain_images
-        },
-        action = { type = "expire" }
-      },
-      {
-        rulePriority = 2,
         description  = "Expire untagged images older than 14 days",
         selection = {
           tagStatus   = "untagged",
           countType   = "sinceImagePushed",
           countUnit   = "days",
           countNumber = 14
+        },
+        action = { type = "expire" }
+      },
+      {
+        rulePriority = 2,
+        description  = "Keep last ${var.retain_images} images",
+        selection = {
+          tagStatus   = "any",
+          countType   = "imageCountMoreThan",
+          countNumber = var.retain_images
         },
         action = { type = "expire" }
       }
